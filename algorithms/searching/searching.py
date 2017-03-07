5edda513ba5b7ec3962cc2eed6cfacf8faebdf4d
Saurav Gupta <saurav2104@gmail.com>
2017-03-07 08:31:42 +0530

    Reformatting

--- algorithms/searching/searching.py
+++ algorithms/searching/searching.py
@@ -1,28 +1,31 @@
 
 def sequential(item, arr):
-	found = False
-	for i in arr:
-		if i == item:
-			found = True
-	return found
+    found = False
+    for i in arr:
+        if i == item:
+            found = True
+    return found
 
 # Divide and Conquer
-# With slice operator in python, binary search does not takes place in O(logn) time. Avoid this by passing start,end
-def binary(item, arr):
-	if len(arr) == 0:
-		return False
-	else:
-		mid = len(arr) // 2
-		if item == arr[mid]:
-			return True
-		else:
-			if item > arr[mid]:
-				return binary(item, arr[mid+1:])
-			else:
-				return binary(item, arr[:mid])
+# With slice operator in python, binary search does not takes place in
+# O(logn) time. Avoid this by passing start,end
 
-#Test Cases
-arr = [45,67,3,2,8,-4,5,2,87]
+
+def binary(item, arr):
+    if len(arr) == 0:
+        return False
+    else:
+        mid = len(arr) // 2
+        if item == arr[mid]:
+            return True
+        else:
+            if item > arr[mid]:
+                return binary(item, arr[mid + 1:])
+            else:
+                return binary(item, arr[:mid])
+
+# Test Cases
+arr = [45, 67, 3, 2, 8, -4, 5, 2, 87]
 assert sequential(64, arr) == False
 assert sequential(-4, arr) == True
 arr.sort()
--- algorithms/sorting/quicksort.py
+++ algorithms/sorting/quicksort.py
@@ -1,37 +1,38 @@
 def quicksort(arr, start, end):
-	if start < end:
-		pivot = _quicksort(arr, start, end)
-		left = quicksort(arr, 0, pivot-1)
-		right = quicksort(arr, pivot+1, end)
-	return arr
+    if start < end:
+        pivot = _quicksort(arr, start, end)
+        left = quicksort(arr, 0, pivot - 1)
+        right = quicksort(arr, pivot + 1, end)
+    return arr
+
 
 def _quicksort(arr, start, end):
-	pivot = start
-	left = start + 1
-	right = end
-	done = False
-	while not done:
-		while arr[left] < arr[pivot]:
-			left += 1
-		while arr[right] > arr[pivot]:
-			right -= 1
-		if left >= right:
-			done = True
-		else:
-			swap(arr, left, right)
-	swap(arr, pivot, right)
-	return right
+    pivot = start
+    left = start + 1
+    right = end
+    done = False
+    while not done:
+        while arr[left] < arr[pivot]:
+            left += 1
+        while arr[right] > arr[pivot]:
+            right -= 1
+        if left >= right:
+            done = True
+        else:
+            swap(arr, left, right)
+    swap(arr, pivot, right)
+    return right
+
 
 def swap(arr, left, right):
-	temp = arr[left]
-	arr[left] = arr[right]
-	arr[right] = temp
+    temp = arr[left]
+    arr[left] = arr[right]
+    arr[right] = temp
 
 
-#Test Cases
-arr = [8,7,6,5,4,3,2,1]
-arr1 = [54,26,93,17,77,31,44,55,20]
-assert sorted(arr1) == quicksort(arr1, 0, len(arr1)-1)
-#Failing case
+# Test Cases
+arr = [8, 7, 6, 5, 4, 3, 2, 1]
+arr1 = [54, 26, 93, 17, 77, 31, 44, 55, 20]
+assert sorted(arr1) == quicksort(arr1, 0, len(arr1) - 1)
+# Failing case
 #assert sorted(arr) == quicksort(arr, 0, len(arr)-1)
-
