0ecb61222bc172afd956e7e0e532c7d7a57df160
Saurav Gupta <saurav2104@gmail.com>
2017-03-09 09:37:51 +0530

    hackerrank: left rotation

---
+++ hackerrank/data_structures/arrays/left_rotation.py
@@ -0,0 +1,24 @@
+""" When we perform  left rotations, the array undergoes the following sequence of changes:
+
+Thus, we print the array's final state as a single line of space-separated values, which is 5 1 2 3 4. """
+
+n, d = map(int, input().split())
+arr = [int(x) for x in input().split()]
+
+# Easy Solution (O(d*n))
+def left_rotate(arr, d, n):
+    for i in range(d):
+        _left_rotate(arr, n)
+    return arr
+
+
+def _left_rotate(arr, n):
+    temp, i = arr[0], 0
+    while i < n - 1:
+        arr[i] = arr[i + 1]
+        i += 1
+    arr[i] = temp
+    return arr
+
+left_rotate(arr, d, n)
+[print(i, end=" ") for i in arr]
