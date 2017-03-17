a66e602fca3901a49c8fafd506f415dd55cfc1d3
Saurav Gupta <saurav.gupta@oracle.com>
2017-03-17 11:52:07 +0530

    Max Heap Implementation

--- data_structures/non-linear/tree/binaryheap.py
+++ data_structures/non-linear/tree/binaryheap.py
@@ -1,30 +1,68 @@
 # Max Heap
 class BinaryHeap:
 
-	def __init__(self):
-		self.list = [0]
-		self.size = 0
+    def __init__(self):
+        self.list = [0]
+        self.size = 0
 
-	def insert(self, element):
-		self.list.append(element)
-		self.size += 1
-		self._insert(self.size)
+    def insert(self, element):
+        self.list.append(element)
+        self.size += 1
+        self.percUp(self.size)
 
-	def _insert(self, size):
-		while size // 2 > 0:
-			if self.list[size] > self.list[size // 2]:
-				self.list[size], self.list[size // 2] = self.list[size // 2], self.list[size]
-		size = size // 2
+    def percUp(self, i):
+        while i // 2 > 0:
+            if self.list[i] > self.list[i // 2]:
+                self.list[i], self.list[
+                    i // 2] = self.list[i // 2], self.list[i]
+            i = i // 2
 
-	def findMax(self):
+    def findMax(self):
+        return self.list[1]
 
-	def delMax(self):
+    def delMax(self):
+        maxval = self.list[1]
+        self.list[1] = self.list[self.size]
+        self.size -= 1
+        self.list.pop()
+        self.percDown(1)
+        return maxval
 
-	def isEmpty(self):
+    def percDown(self, i):
+        while 2 * i <= self.size:
+            mc = self.maxChild(i)
+            if self.list[mc] > self.list[i]:
+                self.list[mc], self.list[i] = self.list[i], self.list[mc]
+            i = mc
 
-	def size(self):
+    def maxChild(self, i):
+        if 2 * i + 1 > self.size:
+            return 2 * i
+        else:
+            if self.list[2 * i] > self.list[2 * i + 1]:
+                return 2 * i
+            else:
+                return 2 * i + 1
 
-	def buildHeap(self):
+    def isEmpty(self):
+        return self.size == 1
 
+    def size(self):
+        return self.size
 
-bh = BinaryHeap([9,5,6,2,3])
+    def buildHeap(self, alist):
+        i = len(alist) // 2
+        self.size = len(alist)
+        self.list = [0] + alist[:]
+        while i > 0:
+            self.percDown(i)
+            i -= 1
+
+# Test Cases
+bh = BinaryHeap()
+bh.buildHeap([9, 5, 6, 2, 3])
+assert bh.findMax() == 9
+bh.insert(10)
+assert bh.findMax() == 10
+bh.delMax()
+assert bh.findMax() == 9
