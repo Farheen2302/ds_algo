237bb1434fe70615c335cafa9aeb9dbda2c4cb49
Saurav Gupta <saurav2104@gmail.com>
2017-03-20 10:14:13 +0530

    BST Implementation

---
+++ data_structures/non-linear/tree/bst/treenode.py
@@ -0,0 +1,42 @@
+class TreeNode:
+
+    def __init__(self, key, val, left=None, right=None, parent=None):
+        self.key = key
+        self.payload = val
+        self.leftChild = left
+        self.rightChild = right
+        self.parent = parent
+
+    def hasLeftChild(self):
+        return self.leftChild
+
+    def hasRightChild(self):
+        return self.rightChild
+
+    def isRoot(self):
+        return self.parent == None
+
+    def isLeftChild(self):
+        return self.parent and self.parent.leftChild == self
+
+    def isRightChild(self):
+        return self.parent and self.parent.rightChild == self
+
+    def isLeaf(self):
+        return not (self.rightChild or self.lefChild)
+
+    def hasAnyChildren(self):
+        return self.rightChild or self.leftChild
+
+    def hasBothChildren(self):
+        return self.rightChild and self.leftChild
+
+    def replaceNodeData(self, key, val, lc , rc):
+        self.key = key
+        self.val = val
+        self.lefChild = lc
+        self.rightChild = rc
+        if self.hasLeftChild():
+            self.leftChild.parent = self
+        if self.hasRightChild():
+            self.parent.right = self
