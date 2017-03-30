96e5234fd9924d26d027e7229ffa09d4bbbd5de4
Saurav Gupta <saurav.gupta@oracle.com>
2017-03-30 18:54:48 +0530

    knight's tour

--- data_structures/non-linear/graph/knights_tour.py
+++ data_structures/non-linear/graph/knights_tour.py
@@ -1,6 +1,35 @@
 from pythonds.graphs import Graph
 
+
+def getNodeId(row, col, bdsize):
+    return (row * bdsize) + col
+
+
+def islegal(x, bdsize):
+    return (x >= 0 and x < bdsize)
+
+
+def getLegalMoves(row, col, bdsize):
+    legalMoves = []
+    possiblities = [(1, 2), (1, -2), (-1, 2), (-1, -2),
+                    (2, 1), (2, -1), (-2, 1), (-2, -1)]
+    for i in possiblities:
+        x = row + i[0]
+        y = col + i[1]
+        if islegal(x,bdsize) and islegal(y, bdsize):
+        	legalMoves.append((x,y))
+    return legalMoves
+
+
 def buildGraph(bdsize):
-	for row in range(bdsize):
-		for col in range(bdsize):
-			
+    ktGraph = Graph()
+    for row in range(bdsize):
+        for col in range(bdsize):
+            nodeId = getNodeId(row, col, bdsize)
+            legalMoves = getLegalMoves(row, col, bdsize)
+            for options in legalMoves:
+            	nid = getNodeId(options[0], options[1], bdsize)
+            	ktGraph.addEdge(nodeId, nid)
+    return ktGraph
+
+buildGraph(8)
