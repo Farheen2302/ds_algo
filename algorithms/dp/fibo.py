03cab006fa4e8a9eabd1fcdb9893cbab8ec569ee
Saurav Gupta <saurav2104@gmail.com>
2017-04-02 17:32:07 +0530

    DP: Bottom's up in fibonacci

--- algorithms/dp/fibo.py
+++ algorithms/dp/fibo.py
@@ -1,3 +1,5 @@
+# DP with memoization
+
 def memo_fibo(n, result):
 	if n in result.keys():
 		return result[n]
@@ -8,4 +10,15 @@ result = {
 	0: 0,
 	1: 1
 }
+
+
+# But sometimes, it's better to go for bottom's up
+
+def dp_bu_fibo(n):
+	result = {0: 0, 1: 1}
+	for i in range(2, n+1):
+		result[i] = result[i-1] + result[i-2]
+	return result[n]
+
 assert (memo_fibo(10, result)) == 55
+assert dp_bu_fibo(10) == 55
