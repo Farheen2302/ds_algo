340209567377e6f783b8a6c1ebaa7724ea387b29
Saurav Gupta <saurav2104@gmail.com>
2017-04-03 09:26:01 +0530

    DP: Coin change

--- hackerrank/algorithms/dp/coin_change.py
+++ hackerrank/algorithms/dp/coin_change.py
@@ -1,17 +1,30 @@
 def getRecursiveChange(n, arr, index, known):
-	if n == 0:
-		return 1
-	if index >= len(arr):
-		return 0
-	key = str(n) + '-' + str(index)
-	if key in known.keys():
-		return known[key]
-	amtWithCoins = 0
-	ways = 0
-	while amtWithCoins <= n:
-		ways += getRecursiveChange(n - amtWithCoins, arr, index + 1, known)
-		known[n] = ways
-		amtWithCoins += arr[index]
-	return ways
- 
-print(getRecursiveChange(10, [2, 5, 3, 6], 0, {}))
+    if n == 0:
+        return 1
+    if n < 0:
+        return 0
+    if n > 0 and index == len(arr):
+        return 0
+    return getRecursiveChange(n - arr[index], arr, index, known) + getRecursiveChange(n, arr, index + 1, known)
+
+assert (getRecursiveChange(10, [2, 5, 3, 6], 0, {})) == 5
+
+# Bottom's up DP approach
+
+
+def dp_coin_changes(amt, coins):
+    result = [[-1 for x in range(amt + 1)] for y in range(len(coins) + 1)]
+    for i in range(len(coins) + 1):
+        result[i][0] = 1
+    for i in range(1, amt + 1):
+        result[0][i] = 0
+    for i in range(1, len(coins) + 1):
+        for j in range(1, amt + 1):
+            if coins[i - 1] <= j:
+                result[i][j] = result[i - 1][j] + result[i][j - coins[i - 1]]
+            else:
+                result[i][j] = result[i - 1][j]
+    return (result[len(coins)][amt])
+
+
+print(dp_coin_changes(5, [1, 2, 3]))
